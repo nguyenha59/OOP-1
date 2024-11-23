feb3e094b4a5cde61225a7bfc01f1160931c5b04
Nguyễn Hạ <166274048+nguyenha59@users.noreply.github.com>
2024-11-23 13:54:12 +0700

    Update and rename Assignment2.java to Assignment2b.java

--- Assignment2b.java
+++ Assignment2b.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Assignment2 {
+public class Assignment2b {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
